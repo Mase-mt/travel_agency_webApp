d81b9584e195fcdc82266d5d354bcb7ee4045b19
Masamo Mathewos <masamomathewos9@gmail.com>
2025-08-03 19:45:19 -0700

    Fixing bug

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/react-router";
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
- dsn: import.meta.env.VITE_SENTRY_AUTH_TOKEN,
+ dsn: "https://e44c036f9e62d66a5f45bf88c10d942a@o4509767771291648.ingest.us.sentry.io/4509767772798976",
  
  // Adds request headers and IP for users, for more info visit:
  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
