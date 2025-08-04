ed6b2591e52d0771676c91e8c6ede9ef633f6837
Masamo Mathewos <masamomathewos9@gmail.com>
2025-08-03 19:20:37 -0700

    Again fixing issue with sentry

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,11 +1,11 @@
-import * as Sentry from "@sentry/react-router";
+ import * as Sentry from "@sentry/react-router";
  import { startTransition, StrictMode } from "react";
  import { hydrateRoot } from "react-dom/client";
  import { HydratedRouter } from "react-router/dom";
 
 Sentry.init({
- dsn: "https://e44c036f9e62d66a5f45bf88c10d942a@o4509767771291648.ingest.us.sentry.io/4509767772798976",
-//  import.meta.env.VITE_SENTRY_AUTH_TOKEN,
+ dsn: import.meta.env.VITE_SENTRY_AUTH_TOKEN,
+ //tried to fix issue with sentry
  // Adds request headers and IP for users, for more info visit:
  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
  sendDefaultPii: true,
